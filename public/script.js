10bc02f4b8a07dd8ce67cea8233bd267b3b475e9
DevArtist <kevinwasonga415@outlook.com>
2025-08-13 05:48:25 +0300

    dice

--- public/script.js
+++ public/script.js
@@ -1,1 +1,42 @@
+let activePlayer = 1;
+let score = 0;
+let scoreP1 = 0;
+let scoreP2 = 0;
 
+const roll = document.getElementById("rollDice");
+const hold = document.getElementById("hold");
+const dice = document.getElementById("dice");
+const p1 = document.querySelector(".player1");
+const p2 = document.querySelector(".player2");
+
+function rollDice() {
+  const diceFace = Math.floor(Math.random() * 6 + 1);
+  dice.src = `/images/dice-${diceFace}.png`;
+  score =+ diceFace;
+}
+
+const updateDisplayPlayer = function () {
+  p1.classList.toggle("active");
+  p2.classList.toggle("active");
+
+  if (activePlayer === 1) {
+    activePlayer = 0;
+  } else {
+    activePlayer = 1;
+  }
+};
+
+const updateCurrentScore = function () {
+  rollDice();
+  if (score !== 1) {
+    document.querySelector(
+      ".active"
+    ).children[1].children[1].innerHTML = `${score}`;
+  } else {
+    updateDisplayPlayer();
+  }
+};
+
+roll.addEventListener("click", updateCurrentScore);
+
+hold.addEventListener("click", updateDisplayPlayer);
